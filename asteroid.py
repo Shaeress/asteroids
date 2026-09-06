30fbc8a3078cdfd7c23b00ab185ab15e64837b6c
Shaeress <shaeress@outlook.com>
2026-09-06 17:43:44 +0200

    Shooting basics implemented

--- asteroid.py
+++ asteroid.py
@@ -3,17 +3,17 @@ from circleshape import CircleShape
 from constants import LINE_WIDTH
 
 class Asteroid(CircleShape):
-	def __init__(self, x: float, y: float, radius: float) -> None:
-		super().__init__(x,y,radius)
+    def __init__(self, x: float, y: float, radius: float) -> None:
+        super().__init__(x,y,radius)
 
-	def draw(self,screen) -> None:
-		pygame.draw.circle(
-			screen,
-			"white",
-			self.position,
-			self.radius,
-			LINE_WIDTH
-		)
+    def draw(self,screen) -> None:
+        pygame.draw.circle(
+            screen,
+            "white",
+            self.position,
+            self.radius,
+            LINE_WIDTH
+        )
 
-	def update(self, dt: float) -> None:
-		self.position += (self.velocity * dt)
+    def update(self, dt: float) -> None:
+        self.position += (self.velocity * dt)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame, sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state, log_event
 from player import Player
+from shot import Shot
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 
@@ -13,9 +14,11 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
 
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, updatable, drawable)
 
     asteroids = pygame.sprite.Group()
     Asteroid.containers = (asteroids, updatable, drawable)
