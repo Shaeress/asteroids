302b78caeeb9d919755df5b38d90239652ccde6c
Shaeress <shaeress@outlook.com>
2026-09-06 17:01:10 +0200

    Asteroid spawning basic implementation

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -14,6 +16,12 @@ def main():
     Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
     
     while True:
         log_state()
